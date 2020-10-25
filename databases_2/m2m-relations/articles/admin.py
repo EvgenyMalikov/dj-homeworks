eccdb006349f7970432362ffbfe0dbe4515f2e51
Evgeny Malikov <mev@offershub.ru>
2020-10-25 21:11:09 +0500

    m2m-relation, change past HW

--- databases_2/m2m-relations/articles/admin.py
+++ databases_2/m2m-relations/articles/admin.py
@@ -1,8 +1,41 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
 
-from .models import Article
+from .models import Article, Tag, Scope
+
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+                count += 1
+        # raise Exception(count)
+        if count > 1:
+            raise ValidationError('Может быть только один главный тэг')
+        if count == 0:
+            raise ValidationError('Укажите основной раздел')
+        return super().clean()
+
+
+class ScopeInLine(admin.TabularInline):
+    model = Tag.article.through
+    formset = ScopeInlineFormset
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = [
+        ScopeInLine
+    ]
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Scope)
+class CategoryAmin(admin.ModelAdmin):
     pass
--- databases_2/m2m-relations/articles/models.py
+++ databases_2/m2m-relations/articles/models.py
@@ -6,7 +6,7 @@ class Article(models.Model):
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
-    image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
+    image = models.ImageField(null=True, blank=True, verbose_name='Изображение')
 
     class Meta:
         verbose_name = 'Статья'
@@ -14,3 +14,32 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=256)
+    article = models.ManyToManyField(
+        'Article',
+        through='Scope',
+    )
+
+    def __str__(self):
+        return self.title
+
+
+class Scope(models.Model):
+    article = models.ForeignKey(
+        'Article',
+        on_delete=models.CASCADE
+
+    )
+    topic = models.ForeignKey(
+        'Tag',
+        on_delete=models.CASCADE,
+    )
+    is_main = models.BooleanField(
+        default=False
+    )
+
+    def __str__(self):
+        return f'{self.article}-{self.topic}'
